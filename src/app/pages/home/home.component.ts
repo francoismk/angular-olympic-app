958fd5c3a3aedcfe4b55875f03c788873a816560
francoismk <francois.szymczak@gmail.com>
2024-11-10 17:27:38 +0100

    fix: interface correction and prepare data for chart

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { Observable, map, of } from 'rxjs';
 
 import { OlympicCountry } from 'src/app/core/models';
 import { OlympicService } from 'src/app/core/services/olympic.service';
@@ -11,10 +11,24 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
 })
 export class HomeComponent implements OnInit {
   public olympics$: Observable<OlympicCountry[] | null> = of(null);
+  public chartData$: Observable<{country: string, totalMedals: number}[] | null> = of(null);
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
     this.olympics$ = this.olympicService.getOlympics();
+
+    this.chartData$ = this.olympics$.pipe(
+      map((countries) => {
+        console.log('Received countries:', countries);
+        if (!countries) {
+          return []
+        }
+        return countries.map(country =>({
+          country: country.country,
+          totalMedals: country.participations.reduce((acc, participations) => acc + participations.medalsCount, 0)
+        }))
+      })
+    )
   }
 }
